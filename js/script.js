5323801b3368384375e34d0f7228b3f756de1ae2
floregdn <145459039+floregdn@users.noreply.github.com>
2023-12-26 22:31:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// verification formulaire
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerIcon = document.getElementById('burger-icon');
+    const menu = document.getElementById('menu');
+
+     burgerIcon.addEventListener('click', function() {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+     });
+});
+
+// theme sombre
+const themebutton = document.querySelector('.themebutton');
+themebutton.addEventListener('click', () => {
+    const body = document.body;
+
+    body.classList.toggle('dark');
+});
